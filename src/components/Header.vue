d9b34393cc90df8e23d33a6a0d4ab57b55193af3
STUDIO SEMANTICA <m90studio@tutanota.com>
2020-09-17 15:04:01 -0400

    Testing initial changes to Vue component and checking if deployment works

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,75 @@
 <template>
-  <div class="header">
+
+<section class="hero is-success is-fullheight">
+  <!-- Hero head: will stick at the top -->
+  <div class="hero-head">
+    <header class="navbar">
+      <div class="container">
+        <div class="navbar-brand">
+          <a class="navbar-item">
+            <img src="https://bulma.io/images/bulma-type-white.png" alt="Logo">
+          </a>
+          <span class="navbar-burger burger" data-target="navbarMenuHeroC">
+            <span></span>
+            <span></span>
+            <span></span>
+          </span>
+        </div>
+        <div id="navbarMenuHeroC" class="navbar-menu">
+          <div class="navbar-end">
+            <a class="navbar-item is-active">
+              Home
+            </a>
+            <a class="navbar-item">
+              Examples
+            </a>
+            <a class="navbar-item">
+              Documentation
+            </a>
+            <span class="navbar-item">
+              <a class="button is-success is-inverted">
+                <span class="icon">
+                  <i class="fab fa-github"></i>
+                </span>
+                <span>Download</span>
+              </a>
+            </span>
+          </div>
+        </div>
+      </div>
+    </header>
+  </div>
+
+  <!-- Hero content: will be in the middle -->
+  <div class="hero-body">
+    <div class="container has-text-centered">
+      <h1 class="title">
+        Title
+      </h1>
+      <h2 class="subtitle">
+        Subtitle
+      </h2>
+    </div>
+  </div>
+
+  <!-- Hero footer: will stick at the bottom -->
+  <div class="hero-foot">
+    <nav class="tabs is-boxed is-fullwidth">
+      <div class="container">
+        <ul>
+          <li class="is-active"><a>Overview</a></li>
+          <li><a>Modifiers</a></li>
+          <li><a>Grid</a></li>
+          <li><a>Elements</a></li>
+          <li><a>Components</a></li>
+          <li><a>Layout</a></li>
+        </ul>
+      </div>
+    </nav>
+  </div>
+</section>
+
+  <!-- <div class="header">
     <b-navbar>
       <template slot="brand">
         <b-navbar-item tag="router-link" :to="{ path: '/' }">
@@ -26,9 +96,15 @@
         </b-navbar-dropdown>
       </template>
 
-      <template slot="end">
+ <template slot="end"> 
         <b-navbar-item tag="div">
-          <div class="buttons">
+   
+        </b-navbar-item>
+      </template>
+    </b-navbar>
+
+         <template>
+         <div class="buttons">
             <a class="button is-primary">
               <strong>Sign up</strong>
             </a>
@@ -36,11 +112,11 @@
               Log in
             </a>
           </div>
-        </b-navbar-item>
-      </template>
-    </b-navbar>
+              </template>
   </div>
-</template>
+-->
+</template> 
+
 
 <script>
 export default {
