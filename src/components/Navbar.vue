ae4ae0f556497da61cb9456f3c6daf9e850267c8
STUDIO SEMANTICA <m90studio@tutanota.com>
2020-09-19 14:33:24 -0400

    Changed Navbar Options Styling

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -27,15 +27,11 @@
         </template>
 
         <template slot="end">
-            <b-navbar-item tag="div">
-                <div class="buttons">
-                    <a class="button is-primary">
-                        <strong>Sign up</strong>
-                    </a>
-                    <a class="button is-light">
-                        Log in
-                    </a>
-                </div>
+            <b-navbar-item href="#">
+                Home
+            </b-navbar-item>
+            <b-navbar-item href="#">
+                Documentation
             </b-navbar-item>
         </template>
     </b-navbar>
